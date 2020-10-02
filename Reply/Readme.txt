2794d6a82545eacde2329746da39360f217efd12
Grecu Andrei-George <50420491+grecuandrei@users.noreply.github.com>
2020-10-02 12:50:51 +0300

    Updated script+ Readme

--- Reply/Readme.txt
+++ Reply/Readme.txt
@@ -1,3 +1,8 @@
+Scriptul sterge emailurile din campania aleasa.
+
 Campania trebuie sa aiba numele legat sau cu underscore: Nume_Campanie.
 
-Rularea este afisata in linia de comanda in poza.
+fisier_cu_domenii_de_sters.csv contine intr-o coloana toate domeniile care trebuie sterse din Nume_Campanie.
+Exemplu: test1.csv
+
+Rularea este: python3 reaply.py Nume_Campanie fisier_cu_domenii_de_sters.csv fisier_cu_toate_emailurile_de_pe_reply.csv
--- Reply/reaply.py
+++ Reply/reaply.py
@@ -18,21 +18,29 @@ def main(argv):
     
     companyId = response.json()["id"]
     
-    print(argv[1]+".csv")
+    print(argv[1])
+    print(argv[2])
     
-    df = pd.read_csv(argv[1]+".csv")
+    df = pd.read_csv(argv[1]) # domeniile date
+    df2 = pd.read_csv(argv[2]) # lista cu toate mailurile
+    df2 = df2[['Email']]
     for index, row in df.iterrows():
-        url = "https://api.reply.io/v1/actions/removepersonfromcampaignbyid"
+      for index1, row1 in df2.iterrows():
+        dom = '@' + str(row[0])
+        if dom in str(row1[0]):
+          print(str(row1[0]))
 
-        payload = "{\r\n\"campaignId\": " + str(companyId) + ",\r\n\"email\": \"" + str(row[0]) + "\"\r\n}"
-        headers = {
-          'Content-Type': 'application/json',
-          'x-api-key': 'QJt4ZhWZ7IEXPhPNbIDlHxGB'
-        }
-        
-        response = requests.request("POST", url, headers=headers, data = payload)
-        
-        print(response.status_code)
+          url = "https://api.reply.io/v1/actions/removepersonfromcampaignbyid"
+
+          payload = "{\r\n\"campaignId\": " + str(companyId) + ",\r\n\"email\": \"" + str(row1[0]) + "\"\r\n}"
+          headers = {
+            'Content-Type': 'application/json',
+            'x-api-key': 'QJt4ZhWZ7IEXPhPNbIDlHxGB'
+          }
+          
+          response = requests.request("POST", url, headers=headers, data = payload)
+          
+          print(response.status_code)
     
     
 if __name__=="__main__":
